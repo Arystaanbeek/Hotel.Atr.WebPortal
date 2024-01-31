276433442061e10d3e8f4b70f2b02e20e7631eaa
Mukhammed <arystaanbeek@gmail.com>
2024-01-31 10:28:44 +0600

    Add project files.

---
+++ Hotel.Atr.WebPortal/Controllers/RoomsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Atr.WebPortal.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
